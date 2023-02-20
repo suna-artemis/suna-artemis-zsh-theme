1c996f526555ee1c9863fd48568abf14142bc1a5
suna-artemis <125189046+suna-artemis@users.noreply.github.com>
2023-02-20 15:13:50 +0800

    fix🐛:  emmmmm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 THEME_NAME='suna-artemis'
 
-wget -O- $HOME/.oh-my-zsh/themes/$THEME_NAME.zsh-theme https://raw.githubusercontent.com/suna-artemis/suna-artemis-zsh-theme/master/suna-artemis.zsh-theme
+wget -O $HOME/.oh-my-zsh/themes/$THEME_NAME.zsh-theme https://raw.githubusercontent.com/suna-artemis/suna-artemis-zsh-theme/master/suna-artemis.zsh-theme
 sed -i "s/^ZSH_THEME=\".*\"$/ZSH_THEME=\"$THEME_NAME\"/" $HOME/.zshrc
 
 zsh
